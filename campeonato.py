2bace1aa021f5990298f99abe36e716ed4886892
Bea <67487622+Mantovaniz@users.noreply.github.com>
2022-10-23 21:36:50 -0300

    Add files via upload
    Sistema para divisão de chaves para campeonato

---
+++ campeonato.py
@@ -0,0 +1,19 @@
+quantTimes = 0
+quantChaves = 0
+cont = 0
+
+times = []
+chaves = []
+
+quantTimes = int(input("Digite a quantidade de times que deseja cadastrar: "))
+
+for i in range(quantTimes):
+    times.append(str(input("Digite o nome do time: ")))
+
+print("\nTimes cadastrados: " , times)
+
+quantChaves = int(quantTimes/2)
+
+print("\nChaves criadas: ")
+splited = [times[i::quantChaves] for i in range(quantChaves)]
+print(splited)
